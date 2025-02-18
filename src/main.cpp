9332381c2a430d355eea6f77eba0e14268bf5d14
manechkaa <mariatestova5@gmail.com>
2025-02-18 20:54:21 +0300

    add functions and create СMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by maria on 18.02.2025.
 //
+
+#include <iostream>
+#include "utils.cpp"
+
+int main() {
+    int a = 3;
+    int b = 5;
+    std::cout << sum(a, b) << '\n';
+    std::cout << mult(4, 8) << '\n';
+    std::cout << isprime(7) << " " << isprime(8);
+}
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,15 @@
 //
 // Created by maria on 18.02.2025.
 //
+int sum(int a, int b) {
+    return a + b;
+}
+int mult(int a, int b) {
+    return a * b;
+}
+bool isprime(int a) {
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i == 0) return false;
+    }
+    return true;
+}
