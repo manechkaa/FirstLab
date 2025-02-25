a0ec00a1954a6e113be486dc5038aa303c414d6a
manechkaa <mariatestova5@gmail.com>
2025-02-25 15:48:13 +0300

    delete directories

--- cmake-build-debug/CMakeFiles/FirstLab.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FirstLab.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/FirstLab.dir/src/utils.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FirstLab.dir/src/utils.cpp.obj: \
- C:\Users\Professional\Documents\GitHub\FirstLab\src\utils.cpp
